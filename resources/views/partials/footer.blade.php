f55ef3c370d3f248844c5ab88d47b9b356bb43bf
Nemanja Avramovic <avramyu@gmail.com>
2021-03-27 14:50:33 +0100

    fix: format footer numbers

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,8 +3,11 @@
         <p>
             Copyright &copy; 2017-{{ date('Y') }}, <a href="https://avramovic.info" target="_blank">Nemanja Avramovic</a> a.k.a. <a href="/sgt-baker">Sgt_Baker</a>
         </p>
+        @php
+        $formatter = new \PRStats\Models\Player();
+        @endphp
         <div class="credits">
-            Tracking {{ \PRStats\Models\Player::count() }} players and {{ \PRStats\Models\Clan::count() }} clans in {{ \PRStats\Models\Match::count() }} matches across {{ \PRStats\Models\Server::count() }} servers.
+            Tracking {{ $formatter->formatValue(\PRStats\Models\Player::count()) }} players and {{ $formatter->formatValue(\PRStats\Models\Clan::count()) }} clans in {{ $formatter->formatValue(\PRStats\Models\Match::count()) }} matches across {{ $formatter->formatValue(\PRStats\Models\Server::count()) }} servers.
         </div>
 {{--        <div class="credits">--}}
 {{--            Created with Dashio template by <a href="https://templatemag.com/">TemplateMag</a>--}}
